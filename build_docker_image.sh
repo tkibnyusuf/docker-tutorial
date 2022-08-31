dba034a30afe7f00d3bc5b114fedbdaad74c5835
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-08-31 15:16:33 -0500

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # create a repository to store the docker image in docker hub
 
 # launch an ec2 instance. open port 80 and port 22
@@ -7,27 +9,24 @@ sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-docker info
 
 # create a dockerfile
-sudo vi Dockerfile 
 
 # build the docker image
-docker build -t techmax .
+sudo docker build -t techmax .
 
 # login to your docker hub account
-docker login --username aosnotes77 
+cat ~/docker_password.txt | sudo docker login --username tkibnyusuf --password-stdin
 
 # use the docker tag command to give the image a new name
-docker tag techmax aosnotes77/techmax
+sudo docker tag techmax tkibnyusuf/project3
 
 # push the image to your docker hub repository
-docker push aosnotes77/techmax
-
+sudo docker push tkibnyusuf/project3
 # start the container to test the image 
-docker run -dp 80:80 aosnotes77/techmax
+sudo docker run -dp 80:80 tkibnyusuf/project3
 
-# references
+# referances
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/create-container-image.html
 # https://docs.docker.com/get-started/02_our_app/
+# https://docs.docker.com/engine/reference/commandline/login/#provide-a-password-using-stdin
