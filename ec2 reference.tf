5719edabcaeadcaebf4cc81f45d1b044659d7537
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-08-31 15:17:13 -0500

    Update ec2 reference.tf

--- ec2 reference.tf
+++ ec2 reference.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraform-user"
+  profile = "yusuf"
 }
 
 
@@ -32,29 +32,29 @@ resource "aws_default_subnet" "default_az1" {
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22"
-  vpc_id      = 
+  vpc_id      = resource.aws_default_vpc.default_vpc.id
 
   ingress {
     description = "http access"
-    from_port   = 
-    to_port     = 
-    protocol    = 
-    cidr_blocks = 
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "ssh access"
-    from_port   = 
-    to_port     = 
-    protocol    = 
-    cidr_blocks = 
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 
-    to_port     = 
-    protocol    = 
-    cidr_blocks = 
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -82,11 +82,11 @@ data "aws_ami" "amazon_linux_2" {
 
 # launch the ec2 instance
 resource "aws_instance" "ec2_instance" {
-  ami                    = 
-  instance_type          = 
-  subnet_id              = 
-  vpc_security_group_ids = 
-  key_name               = 
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = "devopskeypair"
 
   tags = {
     Name = "docker server"
@@ -99,41 +99,43 @@ resource "null_resource" "name" {
 
   # ssh into the ec2 instance 
   connection {
-    type        = 
-    user        = 
-    private_key = file()
-    host        = 
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/Downloads/devopskeypair.pem")
+    host        = aws_instance.ec2_instance.public_ip
   }
 
   # copy the password file for your docker hub account
   # from your computer to the ec2 instance 
   provisioner "file" {
-    source      = 
-    destination = 
+    source      = "~/Downloads/docker_password.txt"
+    destination = "/home/ec2-user/docker_password.txt"
   }
 
   # copy the dockerfile from your computer to the ec2 instance 
   provisioner "file" {
-    source      = 
-    destination = 
+    source      = "Dockerfile"
+    destination = "/home/ec2-user/Dockerfile"
   }
 
   # copy the build_docker_image.sh from your computer to the ec2 instance 
   provisioner "file" {
-    source      = 
-    destination = 
+    source      = "build_docker_image.sh"
+    destination = "/home/ec2-user/build_docker_image.sh"
   }
 
   # set permissions and run the build_docker_image.sh file
   provisioner "remote-exec" {
     inline = [
       
-      
+       "sudo chmod +x /home/ec2-user/build_docker_image.sh",
+       "sh /home/ec2-user/build_docker_image.sh",
+
     ]
   }
 
   # wait for ec2 to be created
-  depends_on = []
+  depends_on = [aws_instance.ec2_instance]
 
 }
 
